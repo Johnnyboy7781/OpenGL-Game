acfcd28b47071406987246ccae3011e98e09c548
Jonathan McDonnell <mcdonnell.jonathan@yahoo.com>
2022-09-26 19:50:52 -0400

    Link shaders, refactor error checking

--- OpenGL-Game/main.cpp
+++ OpenGL-Game/main.cpp
@@ -16,6 +16,12 @@ const char* fragmentShaderSource = "#version 400 core\n"
 	" FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 	"}\n";
 
+float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	0.5f, -0.5f, 0.0f,
+	0.0f, 0.5f, 0.0f
+};
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
@@ -27,28 +33,31 @@ void processInput(GLFWwindow* window)
 		glfwSetWindowShouldClose(window, true);
 }
 
-void checkCompilationErrors(unsigned int shader)
+void checkCompilationErrors(unsigned int object, bool isShader = true)
 {
 	int success;
 	char infoLog[512];
-	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	isShader
+		? glGetShaderiv(object, GL_COMPILE_STATUS, &success)
+		: glGetProgramiv(object, GL_COMPILE_STATUS, &success);
 
 	if (!success)
 	{
-		glGetShaderInfoLog(shader, 512, NULL, infoLog);
-		std::cout << "Shader compilation failed\n" << infoLog << std::endl;
+		isShader
+			? glGetShaderInfoLog(object, 512, NULL, infoLog)
+			: glGetProgramInfoLog(object, 512, NULL, infoLog);
+		std::cout << "Compilation failed\n" << infoLog << std::endl;
 	}
 }
 
 int main()
 {
-	// Init GLFW
+	// Initialization
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// Create window w/ options
 	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Game", NULL, NULL);
 	if (window == NULL)
 	{
@@ -58,49 +67,49 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 
-	// Init Glad
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
 
-	// Tell OpenGL size of rendering window
 	glViewport(0, 0, 800, 600);
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
-	};
-
-	// Generate vertex buffer object (OBJ)
+	// Shader creation
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);
-	// Bind buffer to buffer arr
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// Create vertex shader
 	unsigned int vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	// Attach source code to vertex shader
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
 
-	// Check for errors
 	checkCompilationErrors(vertexShader);
 
-	// Create fragment shader
 	unsigned int fragmentShader;
 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
 
-	// Check for errors
 	checkCompilationErrors(fragmentShader);
+
+	// Link shaders
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	checkCompilationErrors(shaderProgram, false);
+
+	glUseProgram(shaderProgram);
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
 	// Render loop
 	while (!glfwWindowShouldClose(window))
@@ -110,9 +119,7 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// Swaps back and front buffers
 		glfwSwapBuffers(window);
-		// Checks for events, updates state, and calls corresponding functions
 		glfwPollEvents();
 	}
 
