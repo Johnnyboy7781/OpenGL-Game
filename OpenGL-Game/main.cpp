aa7feb9eabaf014910d38e373d5b04fee610a448
Jonathan McDonnell <mcdonnell.jonathan@yahoo.com>
2022-09-26 19:29:11 -0400

    Compile vertex shader

--- OpenGL-Game/main.cpp
+++ OpenGL-Game/main.cpp
@@ -2,6 +2,20 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+const char* vertexShaderSource = "#version 400 core\n"
+	"layout (location = 0) in vec3 aPos;\n"
+	"void main()\n"
+	"{\n"
+	" gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"}\0";
+
+const char* fragmentShaderSource = "#version 400 core\n"
+	"out vec4 FragColor\n"
+	"void main()\n"
+	"{\n"
+	" FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"}\n";
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
@@ -20,9 +34,9 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	
+
 	// Create window w/ options
-	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Game", NULL, NULL); 
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Game", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create window" << std::endl;
@@ -32,7 +46,7 @@ int main()
 	glfwMakeContextCurrent(window);
 
 	// Init Glad
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
@@ -42,6 +56,38 @@ int main()
 	glViewport(0, 0, 800, 600);
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+
+	// Generate vertex buffer object (OBJ)
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	// Bind buffer to buffer arr
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Create vertex shader
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	// Attach source code to vertex shader
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	// Check for errors
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
 
 	// Render loop
 	while (!glfwWindowShouldClose(window))
