b17419e76e60b89f619958fe881fb9d8782b2e8a
Jonathan McDonnell <mcdonnell.jonathan@yahoo.com>
2022-09-27 16:35:22 -0400

    Refactor

--- OpenGL-Game/main.cpp
+++ OpenGL-Game/main.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+void checkCompilationErrors(unsigned int object, bool isShader = true);
 
 const char* vertexShaderSource = "#version 400 core\n"
 	"layout (location = 0) in vec3 aPos;\n"
@@ -9,40 +14,12 @@ const char* vertexShaderSource = "#version 400 core\n"
 	" gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 	"}\0";
 
-const char* fragmentShaderSource = "#version 400 core\n"
+const char* fragmentShaderSrc = "#version 400 core\n"
 	"out vec4 FragColor;\n"
 	"void main()\n"
 	"{\n"
 	" FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 	"}\n";
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-	glViewport(0, 0, width, height);
-}
-
-void processInput(GLFWwindow* window)
-{
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
-}
-
-void checkCompilationErrors(unsigned int object, bool isShader = true)
-{
-	int success;
-	char infoLog[512];
-	isShader
-		? glGetShaderiv(object, GL_COMPILE_STATUS, &success)
-		: glGetProgramiv(object, GL_COMPILE_STATUS, &success);
-
-	if (!success)
-	{
-		isShader
-			? glGetShaderInfoLog(object, 512, NULL, infoLog)
-			: glGetProgramInfoLog(object, 512, NULL, infoLog);
-		std::cout << "Compilation failed\n" << infoLog << std::endl;
-	}
-}
 
 int main()
 {
@@ -78,9 +55,8 @@ int main()
 
 	checkCompilationErrors(vertexShader);
 
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSrc, NULL);
 	glCompileShader(fragmentShader);
 
 	checkCompilationErrors(fragmentShader);
@@ -97,26 +73,28 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	// VAO/VBO creation
-	float vertices[] = {
+	float triangle[] = {
 		-0.5f, -0.5f, 0.0f,
 		0.5f, -0.5f, 0.0f,
 		0.0f, 0.5f, 0.0f
 	};
+	unsigned int indices[] = {
+		0, 1, 3,
+		1, 2, 3
+	};
+
+	// VAO/VBO creation
 	unsigned int VAO, VBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+
 	glBindVertexArray(VAO);
-
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
+	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	glBindVertexArray(0);
+	// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	// Render loop
 	while (!glfwWindowShouldClose(window))
@@ -140,4 +118,32 @@ int main()
 
 	glfwTerminate();
 	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+void checkCompilationErrors(unsigned int object, bool isShader)
+{
+	int success;
+	char infoLog[512];
+	isShader
+		? glGetShaderiv(object, GL_COMPILE_STATUS, &success)
+		: glGetProgramiv(object, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		isShader
+			? glGetShaderInfoLog(object, 512, NULL, infoLog)
+			: glGetProgramInfoLog(object, 512, NULL, infoLog);
+		std::cout << "Compilation failed\n" << infoLog << std::endl;
+	}
 }
